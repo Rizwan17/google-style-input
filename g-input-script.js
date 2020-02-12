ef2320e5f0a3f9d28921a4c293b54649c0437985
Rizwan Khan <rizwankhan.august16@gmail.com>
2020-02-12 11:15:46 -0800

    google style input example

---
+++ g-input-script.js
@@ -0,0 +1,21 @@
+var gInputContainer = document.getElementsByClassName("g-input-container");
+
+for(var i=0; i<gInputContainer.length; i++){
+
+	var gInput = gInputContainer[i].getElementsByTagName("input")[0];
+	var label = gInputContainer[i].getElementsByTagName("label")[0];
+	gInput.addEventListener("focus", addGEffect.bind(this, gInputContainer[i]));
+
+}
+
+
+function addGEffect(gInput){
+	for(var i=0; i<gInputContainer.length; i++){
+  	if(gInputContainer[i].getElementsByTagName("input")[0].value == ""){
+    	gInputContainer[i].classList.remove('active');
+    }else{
+    	gInputContainer[i].classList.add('active');
+    }
+  }
+  gInput.classList.add('active');
+}
